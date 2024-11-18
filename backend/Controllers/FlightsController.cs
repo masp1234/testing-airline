51aa63969db33f90d435a1dbb817b7f945859eff
August Dedenroth <augu0577@stud.kea.dk>
2024-11-18 17:41:00 +0100

    Create role authorization

--- backend/Controllers/FlightsController.cs
+++ backend/Controllers/FlightsController.cs
@@ -1,66 +1,68 @@
 ﻿using backend.Dtos;
 using backend.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace backend.Controllers
 {
-    [ApiController]
-    [Route("/api/mysql/[controller]")]
-    public class FlightsController(IFlightService flightService) : ControllerBase
-    {
-        private readonly IFlightService _flightService = flightService;
+	[ApiController]
+	[Route("/api/mysql/[controller]")]
+	public class FlightsController(IFlightService flightService) : ControllerBase
+	{
+		private readonly IFlightService _flightService = flightService;
 
-        [HttpGet]
-        public async Task<IActionResult> GetAllFlights()
-        {
-            try
-            {
-                var flights = await _flightService.GetAllFlights();
-                return Ok(flights);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-                SentrySdk.CaptureException(ex);
-                return StatusCode(StatusCodes.Status500InternalServerError, new { message = "An error occured while trying to get flights." });
-            }
-        }
+		[HttpGet]
+		public async Task<IActionResult> GetAllFlights()
+		{
+			try
+			{
+				var flights = await _flightService.GetAllFlights();
+				return Ok(flights);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex);
+				SentrySdk.CaptureException(ex);
+				return StatusCode(StatusCodes.Status500InternalServerError, new { message = "An error occured while trying to get flights." });
+			}
+		}
 
-        [HttpPost]
-        public async Task<IActionResult> AddFlight([FromBody] FlightCreationRequest flightCreationRequest)
-        {
-            try
-            {
-                var createdFlight = await _flightService.CreateFlight(flightCreationRequest);
-                return StatusCode(StatusCodes.Status201Created, new { message = "Flight was created successfully!", createdFlight });
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-                SentrySdk.CaptureException(ex);
-                return StatusCode(StatusCodes.Status500InternalServerError, new { message = "An error occured while trying to create a new flight." });
-            }
-        }
+		[Authorize (Roles = "Admin")]
+		[HttpPost]
+		public async Task<IActionResult> AddFlight([FromBody] FlightCreationRequest flightCreationRequest)
+		{
+			try
+			{
+				var createdFlight = await _flightService.CreateFlight(flightCreationRequest);
+				return StatusCode(StatusCodes.Status201Created, new { message = "Flight was created successfully!", createdFlight });
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex);
+				SentrySdk.CaptureException(ex);
+				return StatusCode(StatusCodes.Status500InternalServerError, new { message = "An error occured while trying to create a new flight." });
+			}
+		}
 
-        [HttpGet("search")]
-        public async Task<IActionResult> GetFlightsByDepartureDestinationAndDepartureDate([FromQuery] int departureAirportId,
-                                                                                          [FromQuery] int destinationAirportId,
-                                                                                          [FromQuery] DateOnly departureDate)
-        {
-            if (departureAirportId == 0 || destinationAirportId == 0 || departureDate == DateOnly.MinValue)
-            {
-                return BadRequest(new { message = "The request is missing either departureAirportId, destinationAirportId, departureDate or all of these parameters." });
-            }
-            try
-            {
-                var flights = await _flightService.GetFlightsByDepartureDestinationAndDepartureDate(departureAirportId, destinationAirportId, departureDate);
-                return Ok(new { flights });
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-                return StatusCode(StatusCodes.Status500InternalServerError, new { message = "An error occured while trying to get flights by departure, destination and departure date." });
-            }
-        }
-    }
+		[HttpGet("search")]
+		public async Task<IActionResult> GetFlightsByDepartureDestinationAndDepartureDate([FromQuery] int departureAirportId,
+																						  [FromQuery] int destinationAirportId,
+																						  [FromQuery] DateOnly departureDate)
+		{
+			if (departureAirportId == 0 || destinationAirportId == 0 || departureDate == DateOnly.MinValue)
+			{
+				return BadRequest(new { message = "The request is missing either departureAirportId, destinationAirportId, departureDate or all of these parameters." });
+			}
+			try
+			{
+				var flights = await _flightService.GetFlightsByDepartureDestinationAndDepartureDate(departureAirportId, destinationAirportId, departureDate);
+				return Ok(new { flights });
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex);
+				return StatusCode(StatusCodes.Status500InternalServerError, new { message = "An error occured while trying to get flights by departure, destination and departure date." });
+			}
+		}
+	}
 }
--- backend/Controllers/UserController.cs
+++ backend/Controllers/UserController.cs
@@ -5,88 +5,89 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace backend.Controllers
 {
-    [ApiController]
-    [Route("/api/mysql/[controller]")]
-    public class UsersController : ControllerBase
-    {
-        private readonly IUserService _userService;
-        public UsersController(IUserService userService)
-        {
-            _userService = userService;
-        }
-        [HttpGet]
-        public async Task<IActionResult> GetUsers()
-        {
-            try
-            {
-                var users = await _userService.GetUsers();
-                return Ok(users);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-                return StatusCode(StatusCodes.Status500InternalServerError, new { message = "An error occured while trying to get users." });
+	[ApiController]
+	[Route("/api/mysql/[controller]")]
+	public class UsersController : ControllerBase
+	{
+		private readonly IUserService _userService;
+		public UsersController(IUserService userService)
+		{
+			_userService = userService;
+		}
+		[HttpGet]
+		public async Task<IActionResult> GetUsers()
+		{
+			try
+			{
+				var users = await _userService.GetUsers();
+				return Ok(users);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex);
+				return StatusCode(StatusCodes.Status500InternalServerError, new { message = "An error occured while trying to get users." });
 
-            }
-        }
-        [HttpPost]
-        public async Task<IActionResult> AddUser([FromBody] UserCreationRequest userCreationRequest)
-        {
-            if (string.IsNullOrEmpty(userCreationRequest.Email) || string.IsNullOrEmpty(userCreationRequest.Password))
-            {
-                return BadRequest(new { message = "Email or password missing." });
-            }
-            try
-            {
-                UserResponse? user = await _userService.GetUserByEmail(userCreationRequest.Email);
-                if (user != null)
-                {
-                    return Conflict(new { message = "User already exists with this email." });
-                }
+			}
+		}
+		[HttpPost]
+		public async Task<IActionResult> AddUser([FromBody] UserCreationRequest userCreationRequest)
+		{
+			if (string.IsNullOrEmpty(userCreationRequest.Email) || string.IsNullOrEmpty(userCreationRequest.Password))
+			{
+				return BadRequest(new { message = "Email or password missing." });
+			}
+			try
+			{
+				UserResponse? user = await _userService.GetUserByEmail(userCreationRequest.Email);
+				if (user != null)
+				{
+					return Conflict(new { message = "User already exists with this email." });
+				}
 
-                await _userService.CreateUser(userCreationRequest);
-                return StatusCode(StatusCodes.Status201Created, new { message = "User created successfully!" });
-            }
+				await _userService.CreateUser(userCreationRequest);
+				return StatusCode(StatusCodes.Status201Created, new { message = "User created successfully!" });
+			}
 
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-                SentrySdk.CaptureException(ex);
-                return StatusCode(StatusCodes.Status500InternalServerError, new { message = "An error occurred while trying to create a new user." });
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex);
+				SentrySdk.CaptureException(ex);
+				return StatusCode(StatusCodes.Status500InternalServerError, new { message = "An error occurred while trying to create a new user." });
 
-            }
-        }
-        [HttpPost("login")]
-        
-        public async Task<IActionResult> Login([FromBody] JwtRequest request)
-        {
-            var user = await _userService.CheckUserByEmail(request.Email);
+			}
+		}
+		[HttpPost("login")]
+		
+		public async Task<IActionResult> Login([FromBody] JwtRequest request)
+		{
+			var user = await _userService.CheckUserByEmail(request.Email);
 
-            if (user == null) return NotFound(new {message = "User not exist!"});
+			if (user == null) return NotFound(new {message = "User not exist!"});
 
-            if (user != null)
-            {
-                // Initialize PasswordHasher to verify password
-                Boolean passwordValidation =_userService.CheckPasswordValidation(request.Password, user.Password, user);
-                // Check if the password is correct
-                if (passwordValidation)
-                {
-                    // Generate JWT token after successful password verification
-                    var token = _userService.GenerateJwtToken(user);
+			if (user != null)
+			{
+				// Initialize PasswordHasher to verify password
+				Boolean passwordValidation =_userService.CheckPasswordValidation(request.Password, user.Password, user);
+				// Check if the password is correct
+				if (passwordValidation)
+				{
+					// Generate JWT token after successful password verification
+					var token = _userService.GenerateJwtToken(user);
 
-                    Response.Cookies.Append("AuthToken", token, new CookieOptions
-                    {
-                        HttpOnly = true,
-                        Secure = true,
-                        SameSite = SameSiteMode.Strict
-                    });
+					Response.Cookies.Append("AuthToken", token, new CookieOptions
+					{
+						HttpOnly = true,
+						Secure = true,
+						SameSite = SameSiteMode.Strict,
+						Expires = DateTime.UtcNow.AddHours(1)
+					});
 
-                    return Ok(new { message = "Login successful: " });
-                }
-            }
+					return Ok(new { message = "Login successful: " });
+				}
+			}
 
-            return Unauthorized();
-        }
-    }
+			return Unauthorized();
+		}
+	}
 }
 
