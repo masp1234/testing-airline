c09e4f7742b1a49f5cd43751e9a3cade6afb724f
masp1234 <mart2c04@stud.kea.dk>
2024-11-22 18:09:53 +0100

    add BookingCreationRequest DTO

---
+++ backend/Controllers/BookingsController.cs
@@ -0,0 +1,20 @@
+﻿using backend.Dtos;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [ApiController]
+    [Route("/api/mysql/[controller]")]
+    public class BookingsController: ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> CreateBooking([FromBody] BookingCreationRequest bookingCreationRequest)
+        {
+            Console.WriteLine(bookingCreationRequest.Tickets[0].Passenger.Email);
+
+            return Ok(bookingCreationRequest);
+        }
+
+    }
+}
---
+++ backend/Dtos/BookingCreationRequest.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using backend.Models;
+
+namespace backend.Dtos
+{
+    public class BookingCreationRequest
+    {
+        public int UserId { get; set; }
+
+        public TicketCreationRequest[] Tickets { get; set; }
+
+    }
+
+    public class TicketCreationRequest
+    {
+        public PassengerCreationRequest Passenger { get; set; }
+
+        public int FlightId { get; set; }
+
+        public int FlightClassId { get; set; }
+
+    }
+
+    public class PassengerCreationRequest
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+    }
+}
