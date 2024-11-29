fad997b0ad79698422e904e8ef33dc32f3bc142a
masp1234 <mart2c04@stud.kea.dk>
2024-11-29 19:18:12 +0100

    return BookingCreationResponse DTO in response instead of Booking model

---
+++ backend/Dtos/BookingCreationResponse.cs
@@ -0,0 +1,13 @@
+﻿using backend.Models;
+
+namespace backend.Dtos
+{
+    public class BookingCreationResponse
+    {
+        public int Id { get; set; }
+
+        public string ConfirmationNumber { get; set; } = null!;
+
+        public int UserId { get; set; }
+    }
+}
