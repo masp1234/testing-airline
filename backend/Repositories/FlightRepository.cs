717a1884287891ca8da2036c5bfacf8732d34b6b
masp1234 <mart2c04@stud.kea.dk>
2024-11-29 17:43:04 +0100

    use stored procedure to check for overlapping flights and insert flight

--- backend/Repositories/FlightRepository.cs
+++ backend/Repositories/FlightRepository.cs
@@ -1,6 +1,8 @@
 ﻿using backend.Database;
 using backend.Models;
 using Microsoft.EntityFrameworkCore;
+using MySqlConnector;
+using System.Data;
 
 namespace backend.Repositories
 {
@@ -46,12 +48,56 @@ namespace backend.Repositories
             return flight;
         }
 
-        public async Task<Flight> Create(Flight newFlight)
+        public async Task<Flight> Create(Flight flight)
         {
-            await _context.Flights.AddAsync(newFlight);
-            await _context.SaveChangesAsync();
-            return newFlight;
+            /*
+               Would prefer not to handle connections manually like this, and instead let EF Core handle it all,
+               but this is needed to retrieve the last inserted ID from the stored procedure.
+            */
+
+            /*
+               the 'using' statement makes sure that the database connection is closed/released 
+               to the connection pool after the clode block ends, regardless if an exception happens or not.
+               Its the same as using try, catch and finally, where you then close the connection in the 'finally' block.
+             */
+            using var connection = _context.Database.GetDbConnection();
+            await connection.OpenAsync();
+
+            using var command = connection.CreateCommand();
+            command.CommandText = "CheckAndInsertFlight";
+            command.CommandType = CommandType.StoredProcedure;
+
+            command.Parameters.Add(new MySqlParameter("@airplaneId", flight.FlightsAirplaneId));
+            command.Parameters.Add(new MySqlParameter("@departureTime", flight.DepartureTime));
+            command.Parameters.Add(new MySqlParameter("@completionTime", flight.CompletionTime));
+            command.Parameters.Add(new MySqlParameter("@flightCode", flight.FlightCode));
+            command.Parameters.Add(new MySqlParameter("@departurePort", flight.DeparturePort));
+            command.Parameters.Add(new MySqlParameter("@arrivalPort", flight.ArrivalPort));
+            command.Parameters.Add(new MySqlParameter("@travelTime", flight.TravelTime));
+            command.Parameters.Add(new MySqlParameter("@price", flight.Price));
+            command.Parameters.Add(new MySqlParameter("@kilometers", flight.Kilometers));
+            command.Parameters.Add(new MySqlParameter("@economySeats", flight.EconomyClassSeatsAvailable));
+            command.Parameters.Add(new MySqlParameter("@businessSeats", flight.BusinessClassSeatsAvailable));
+            command.Parameters.Add(new MySqlParameter("@firstClassSeats", flight.FirstClassSeatsAvailable));
+            command.Parameters.Add(new MySqlParameter("@airlineId", flight.FlightsAirlineId));
+            command.Parameters.Add(new MySqlParameter("@idempotencyKey", flight.IdempotencyKey));
+
+            // Output parameter
+            var newFlightIdParam = new MySqlParameter("@newFlightId", MySqlDbType.Int32)
+            {
+                Direction = ParameterDirection.Output
+            };
+            command.Parameters.Add(newFlightIdParam);
+
+            // Execute the command that calls the "CheckAndInsertFlight" stored procedure.
+            await command.ExecuteNonQueryAsync();
+
+            flight.Id = (int)newFlightIdParam.Value;
+
+            return flight;
         }
+
+
 
         public async Task<List<Flight>> GetFlightsByDepartureDestinationAndDepartureDate(int departureAirportId, int destinationAirportId, DateOnly departureDate)
         {
