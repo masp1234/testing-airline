02bbce49efdac7d3c5e8fd81ddf217faa1eb9833
masp1234 <mart2c04@stud.kea.dk>
2024-12-21 10:33:00 +0100

    does not work

---
+++ backend.Tests/Fixtures/TestDatabaseFixture.cs
@@ -0,0 +1,63 @@
+﻿using backend.Database;
+using backend.Models;
+using Microsoft.EntityFrameworkCore;
+using Testcontainers.MySql;
+using Xunit.Abstractions;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+public class TestDatabaseFixture : IAsyncLifetime
+{
+    private readonly MySqlContainer _container;
+
+    public DatabaseContext DbContext { get; private set; }
+    private readonly IMessageSink diagnosticMessageSink;
+
+    public TestDatabaseFixture(IMessageSink logger)
+    {
+        diagnosticMessageSink = logger;
+        _container = new MySqlBuilder()
+            .WithImage("mysql:8.0")
+            .WithDatabase("airline_project_tests")
+            .WithPassword("123123")
+            .WithUsername("testuser")
+            .WithPortBinding(3306)
+            .WithCleanUp(true)
+            .Build();
+    }
+
+    public async Task InitializeAsync()
+    {
+        // TRY THIS https://xunit.net/docs/configuration-files#diagnosticMessages - ADD CONFIG FILE
+        var message = new DiagnosticMessage("Ordered {0} test cases", "test");
+        diagnosticMessageSink.OnMessage(message);
+        await _container.StartAsync();
+
+        var connectionString = _container.GetConnectionString();
+        TestLogger.Log(connectionString);
+
+        var options = new DbContextOptionsBuilder<DatabaseContext>()
+            .UseMySql(connectionString, ServerVersion.AutoDetect(connectionString), options =>
+            {
+                options.EnableRetryOnFailure(maxRetryCount: 5);
+            })
+            .Options;
+
+        DbContext = new DatabaseContext(options);
+        await SeedDatabase();
+    }
+
+    private async Task SeedDatabase()
+    {
+        await DbContext.Airlines.AddAsync(new Airline { Name = "Test airline" });
+        await DbContext.SaveChangesAsync();
+    }
+
+    public async Task DisposeAsync()
+    {
+        await DbContext.DisposeAsync();
+        await _container.DisposeAsync();
+    }
+}
---
+++ backend.Tests/Integration/AirlineServiceTests.cs
@@ -0,0 +1,65 @@
+﻿using AutoMapper;
+using backend.Config;
+using backend.Database;
+using backend.Repositories;
+using backend.Services;
+using Microsoft.EntityFrameworkCore;
+using Testcontainers.MySql;
+using Xunit.Abstractions;
+
+public class AirlineServiceTests(ITestOutputHelper output) : IAsyncLifetime
+{
+    private AirlineService _sut;
+    private readonly MySqlContainer _mySqlContainer = new MySqlBuilder()
+        .WithImage("mysql:8.0")
+        .WithDatabase("airline_project")
+    .WithUsername("testuser")
+    .WithPassword("123123")
+        .WithCommand("--default-authentication-plugin=mysql_native_password")
+    .Build();
+
+    private DatabaseContext _dbContext;
+    private readonly ITestOutputHelper _output = output;
+
+    public async Task DisposeAsync()
+    {
+        await _dbContext.DisposeAsync();
+        await _mySqlContainer.DisposeAsync();
+    }
+
+    [Fact]
+    public async Task GetAllAirlines_ShouldReturn_Airlines()
+    {
+        var airlines = await _sut.GetAllAirlines();
+        Assert.NotEmpty(airlines);
+    }
+
+    public async Task InitializeAsync()
+    {
+        var configuration = new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile(new MappingProfile());
+        });
+
+        await _mySqlContainer.StartAsync();
+
+        var connectionString = $"Server={_mySqlContainer.Hostname};Port={_mySqlContainer.GetMappedPublicPort(3306)};Database=airline_project;Uid=root;Pwd=123123;";
+
+        _output.WriteLine(connectionString);
+
+        var options = new DbContextOptionsBuilder<DatabaseContext>()
+            .UseMySql(connectionString, ServerVersion.AutoDetect(connectionString), options =>
+            {
+                options.EnableRetryOnFailure(maxRetryCount: 2);
+            })
+            .Options;
+
+        _dbContext = new DatabaseContext(options);
+
+        await _dbContext.Database.EnsureCreatedAsync();
+
+        _output.WriteLine("DATABASE CREATED?");
+        IMapper mapper = configuration.CreateMapper();
+        _sut = new AirlineService(new AirlineRepository(_dbContext), mapper);
+    }
+}
